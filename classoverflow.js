ff2d5b10348d7166be4d7ef81ecb308c4fd3182e
Aaron Lin <lin.aaron1@gmail.com>
2015-02-16 17:57:45 -0500

    reverted to nested collections

--- classoverflow.js
+++ classoverflow.js
@@ -1,7 +1,7 @@
 // classoverflow.js
 Errors = new Mongo.Collection("errors");
   // errorID, createdAt
-Hints = new Mongo.Collection("hints");
+// Hints = new Mongo.Collection("hints");
   // errorID, createdAt, hintMsg, upvotes
 
 if (Meteor.isClient) {
@@ -11,9 +11,7 @@ if (Meteor.isClient) {
       return Errors.find({}, {sort: {errorID: 1}});
     },
     hints: function () {
-      //return Errors.find({_id: this.id}, {'hints.hintMsg': 1, 'hints.upvotes': 1, _id:0}).sort({'hints.upvotes': 1});
-      //console.log(this._id)
-      //return Errors.find({_id: this._id}, {'hints': 1, _id:0});
+      return Errors.find({_id: this.id}, {'hints.hintMsg': 1, 'hints.upvotes': 1, _id:0}).sort({'hints.upvotes': 1});
     }
   });
 
@@ -40,42 +38,9 @@ if (Meteor.isClient) {
     },
     "submit .new-hint-entry": function(event) {
       var hint = event.target.text.value;
-      //console.log(event);
-      var errordbkey = this._id;
-      var hintID;
-      Hints.insert({
-        errorID: errordbkey, //this.errorID,
-        hintMsg: hint,
-        upvotes: 0
-      },function (err, object) {
-        hintID = object;
-        //console.log(hintID);
-        //console.log(errordbkey);
-        Errors.update(errordbkey, {$push: {hints: {'hintID':hintID} }});
-      });
+      console.log(event);
 
-
-      //Errors.update(this._id, {$push: {hints: {hintMsg: hint, upvotes: 0}}});
-      /*var row = event.target;
-      while (row.parentNode && row.tagName.toLowerCase()!="tr") {
-        row = row.parentNode;
-      }
-      var errorDocs=Errors.find({"errorID": row.id});
-      alert(errorDocs.next());
-
-      if (errorDocs.count()==1) {
-        var aoeu = errorDocs.toArray()[0];
-        alert(aoeu);
-        aoeu.hints.insert({
-          hintMsg: hint,
-          upvotes:0
-        });
-      }
-      else {
-        alert("Something went wrong!");
-      }
-
-      alert("aoeu");*/
+      Errors.update(this._id, {$push: {hints: {hintMsg: hint, upvotes: 0}}});
 
       event.target.text.value = "";
       return false;
@@ -90,14 +55,10 @@ if (Meteor.isClient) {
     "click .delete": function () {
       Errors.remove(this._id);
     },
-    'click .upvote': function(event, template) {
-      //e.preventDefault();
-      //Meteor.call('upvote', this._id);
-      //Errors.update(this._id, {$inc: {upvotes: 1}});
-      //Errors.update(this._id, {$inc: {hints: {hintMsg: upvotes: 1}}})
-      Errors.update(this._id, {$inc: {hints: {hintMsg: hint, upvotes: 0}}});
-      console.log('upvote',this._id, event, template, this)
-
+    "click .upvote": function() {
+      console.log(this);
+      this.upvotes+=1;
+      //Errors.update({}, {$inc: {upvotes: 1}});
     }
   });
 }
