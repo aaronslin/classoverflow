251f8ebcfff2c399ff1b05ce36ec987068a989be
Aaron Lin <lin.aaron1@gmail.com>
2015-02-17 01:52:32 -0500

    display errors and hints properly with helpers

--- classoverflow.js
+++ classoverflow.js
@@ -17,6 +17,19 @@ if (Meteor.isClient) {
     }
   });
 
+  Template.error_entry.helpers({
+    hintIDs: function () {
+      return Errors.find({"_id": this._id}, {hints:1, _id:0});
+    }
+  });
+
+  Template.hint_entry.helpers({
+    getHintInfo: function(hint) {
+      console.log(Hints.find({"_id":hint}));
+      return Hints.find({"_id": hint});
+    }
+  });
+
   Template.body.events({
     "submit .new-error-entry": function (event) {
       // This function is called when the search for ID/submit new ID blank is submitted
@@ -43,39 +56,18 @@ if (Meteor.isClient) {
       //console.log(event);
       var errordbkey = this._id;
       var hintID;
-      Hints.insert({
-        errorID: errordbkey, //this.errorID,
-        hintMsg: hint,
-        upvotes: 0
-      },function (err, object) {
-        hintID = object;
-        //console.log(hintID);
-        //console.log(errordbkey);
-        Errors.update(errordbkey, {$push: {hints: {'hintID':hintID} }});
+        Hints.insert({
+          parent: errordbkey,
+          hintMsg: hint,
+          upvotes: 0
+        },
+        function (err, object) {
+          hintID = object; // the _id of the hint
+          Errors.update(errordbkey, {$push: {hints: {hintID: hintID}}});
       });
 
 
       //Errors.update(this._id, {$push: {hints: {hintMsg: hint, upvotes: 0}}});
-      /*var row = event.target;
-      while (row.parentNode && row.tagName.toLowerCase()!="tr") {
-        row = row.parentNode;
-      }
-      var errorDocs=Errors.find({"errorID": row.id});
-      alert(errorDocs.next());
-
-      if (errorDocs.count()==1) {
-        var aoeu = errorDocs.toArray()[0];
-        alert(aoeu);
-        aoeu.hints.insert({
-          hintMsg: hint,
-          upvotes:0
-        });
-      }
-      else {
-        alert("Something went wrong!");
-      }
-
-      alert("aoeu");*/
 
       event.target.text.value = "";
       return false;
@@ -87,16 +79,21 @@ if (Meteor.isClient) {
       // Set the checked property to the opposite of its current value
       Errors.update(this._id, {$set: {checked: ! this.checked}});
     },
-    "click .delete": function () {
+    "click .delete_error": function () {
       Errors.remove(this._id);
     },
-    'click .upvote': function(event, template) {
+    "click .delete_hint": function() {
+      //alert(this);
+      //Hints.remove({"_id:": this});
+      //Errors.update(this.parent, {$pull: {'hints': this.hintID}});
+    },
+    "click .upvote": function(event, template) {
       //e.preventDefault();
       //Meteor.call('upvote', this._id);
       //Errors.update(this._id, {$inc: {upvotes: 1}});
       //Errors.update(this._id, {$inc: {hints: {hintMsg: upvotes: 1}}})
-      Errors.update(this._id, {$inc: {hints: {hintMsg: hint, upvotes: 0}}});
-      console.log('upvote',this._id, event, template, this)
+      //Errors.update(this._id, {$inc: {hints: {hintMsg: hint, upvotes: 0}}});
+      //console.log('upvote',this._id, event, template, this)
 
     }
   });
