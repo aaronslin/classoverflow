ae0c168634871e0eb402e8258373568e094eec80
eglassman <elena.glassman@gmail.com>
2015-03-10 23:11:50 -0400

    adjusted for feedback from 6.004 TA

--- classoverflow.js
+++ classoverflow.js
@@ -7,7 +7,7 @@ var scrollLocationPrevious = "#inputErrorCoords"
 
 function scrollAndHighlight(scrollLocation) {
   $(scrollLocationPrevious).removeAttr("style");
-  $(scrollLocation).css("background-color","lightyellow");
+  $(scrollLocation).css("background-color","yellow");
   scrollLocationPrevious = scrollLocation;
   $('html, body').animate({
     scrollTop: $(scrollLocation).offset().top-130
@@ -40,7 +40,7 @@ if (Meteor.isClient) {
 
   Template.error_entry.helpers({
     hintIDs: function () {
-      return Errors.find({"_id": this._id}); 
+      return Errors.find({"_id": this._id});
     },
     sortedHints: function () {
       var hintIDList = this.hints.map(function(hintIDObject) {
@@ -69,12 +69,12 @@ if (Meteor.isClient) {
         return false;
       }
       else if (!parseInt(query1) || !parseInt(query2)) {
-        $("#errorMessage").html("The coordinates 'nodes' and 'time' must be integers.");
+        $("#errorMessage").html("Test number and time values must be integers.");
         $("#errorMessage").css("display","block").delay(2500).fadeOut(2000, "linear");
         return false;
       }
-      var query = Errors.findOne({"errorCoord0": query0, 
-                              "errorCoord1": parseInt(query1), 
+      var query = Errors.findOne({"errorCoord0": query0,
+                              "errorCoord1": parseInt(query1),
                               "errorCoord2": parseInt(query2)});
       //var query_count = query.count();
       //console.log(query_count);
@@ -99,8 +99,8 @@ if (Meteor.isClient) {
       };
 
       event.target.errorCoord0.value = ""; // Clear form
-      event.target.errorCoord1.value = ""; 
-      event.target.errorCoord2.value = ""; 
+      event.target.errorCoord1.value = "";
+      event.target.errorCoord2.value = "";
       document.activeElement.blur();
       return false;                 // Prevent default form submit
     },
